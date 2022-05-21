9f2149a38ff514a2f878a848fe49e264e15acfdd
esthi <hoeuyu@student.ethz.ch>
2022-05-21 15:14:37 +0200

    Create str_sub_sequences.py

---
+++ challenge1/str_sub_sequences.py
@@ -0,0 +1,14 @@
+# Python Implementation of the approach
+import itertools
+
+def Sub_Sequences(STR):
+	epsc = []
+	for l in range(1, len(STR)+1):
+		epsc.append(list(itertools.combinations(STR, l)))
+	for c in epsc:
+		for t in c:
+			print (''.join(t), end =' ')
+
+# Testing with driver code
+if __name__ == '__main__':
+	Sub_Sequences('igor')
